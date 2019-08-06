aee10975aa6b20a1c706d88a35d29259c284d42d
Devansh Vyas <devansh.vyas@solutionanalysts.com>
2019-08-06 13:18:11 +0530

    [Initial-commit] PJSIP Framework added

---
+++ PJSIP/PJSIP.h
@@ -0,0 +1,19 @@
+//
+//  PJSIP.h
+//  PJSIP
+//
+//  Created by Devansh Vyas on 06/08/19.
+//  Copyright © 2019 Devansh Vyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PJSIP.
+FOUNDATION_EXPORT double PJSIPVersionNumber;
+
+//! Project version string for PJSIP.
+FOUNDATION_EXPORT const unsigned char PJSIPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PJSIP/PublicHeader.h>
+
+
