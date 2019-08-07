fae8112385fe0a88c8a283df02fa33a886bc84b3
Devansh Vyas <devansh.vyas@solutionanalysts.com>
2019-08-07 12:39:32 +0530

    Rename

--- PJSIPCarthage/PJSIPCarthage.h
+++ PJSIPCarthage/PJSIPCarthage.h
@@ -15,6 +15,3 @@ FOUNDATION_EXPORT double PJSIPCarthageVersionNumber;
 FOUNDATION_EXPORT const unsigned char PJSIPCarthageVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PJSIPCarthage/PublicHeader.h>
-
-
-#import <PJSIPCarthage/pjsua.h>
