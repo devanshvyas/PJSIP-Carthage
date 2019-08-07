9ecca99756bd95d1bbf71f0bb4eec6959a1e71ed
Devansh Vyas <devansh.vyas@solutionanalysts.com>
2019-08-07 12:13:06 +0530

    Renamed

---
+++ PJSIPCarthage/PJSIPCarthage.h
@@ -0,0 +1,20 @@
+//
+//  PJSIPCarthage.h
+//  PJSIPCarthage
+//
+//  Created by Devansh Vyas on 07/08/19.
+//  Copyright © 2019 Devansh Vyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PJSIPCarthage.
+FOUNDATION_EXPORT double PJSIPCarthageVersionNumber;
+
+//! Project version string for PJSIPCarthage.
+FOUNDATION_EXPORT const unsigned char PJSIPCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PJSIPCarthage/PublicHeader.h>
+
+
+#import <PJSIPCarthage/pjsua.h>
